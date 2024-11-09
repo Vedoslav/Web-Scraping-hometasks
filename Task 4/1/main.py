a7f9946aa88c42525ed0868fbb430d75866b0622
Vedoslav <125702692+Vedoslav@users.noreply.github.com>
2024-11-09 22:46:21 +0200

    Create main.py

---
+++ Task 4/1/main.py
@@ -0,0 +1,8 @@
+import requests
+
+def first_page():
+    url = 'https://www.lejobadequat.com/emplois'
+    resp = requests.get(url, headers={'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36 Edg/130.0.0.0'})
+
+if __name__ == '__main__':
+    first_page()
